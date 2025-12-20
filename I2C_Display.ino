252573098fb05c18ab0fcd8a4725616c18955839
AASTROBOii7 <galamavinash@gmail.com>
2025-12-20 22:23:59 +0530

    Update I2C_Display.ino

--- I2C_Display.ino
+++ I2C_Display.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 int counter = 0;
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2); /*0x27 or 0x3f*/
 void setup() {
   lcd.init();              
   lcd.backlight();          
