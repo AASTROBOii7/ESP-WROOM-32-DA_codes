a3b4c262dd092490cd93290c68a4617f0e5e5c17
AASTROBOii7 <galamavinash@gmail.com>
2025-12-22 19:40:01 +0530

    Create DHT_LCD_Integrated.ino

---
+++ DHT_LCD_Integrated.ino
@@ -0,0 +1,36 @@
+void setup() {
+  Wire.begin(21, 22);   // SDA, SCL
+  lcd.init();
+  lcd.backlight();
+
+  dht.begin();
+
+  lcd.setCursor(0, 0);
+  lcd.print("ESP32 Weather");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature(); // Celsius
+
+  if (isnan(humidity) || isnan(temperature)) {
+    lcd.setCursor(0, 0);
+    lcd.print("Sensor Error");
+    delay(2000);
+    return;
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+  lcd.print(" C");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Hum : ");
+  lcd.print(humidity);
+  lcd.print(" %");
+
+  delay(2000);
+}
